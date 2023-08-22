ead957efe9b286e581efc2eed5c01d648229a519
favourmbata <favourmbata520@gmail.com>
2023-08-22 05:40:39 +0100

    fav

---
+++ src/Chapter4B/GasMileage.java
@@ -0,0 +1,2 @@
+package Chapter4B;public class GasMileage {
+}
--- tdd/AccountTest.java
+++ tdd/AccountTest.java
@@ -1,17 +1,2 @@
-import chapter3.Account;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-//public class AccountTest {
-//    @Test
-//    public  void testthaAccountCanCreate(){
-//        Account fcmb =  new Account("dora fin",1000.00);
-//        assertEquals(fcmb.getName("dora fin",fcmb.getBalance(1000.00)));
-//    }
-//
-//    }
-//
-
-
+package PACKAGE_NAME;public class AccountTest {
+}

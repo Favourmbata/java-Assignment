b3d7859586090a9a9ce84996eb0b576c6273a1ca
favourmbata <favourmbata520@gmail.com>
2023-09-27 15:54:08 +0100

    favour

--- src/Chibuzor/LargestScore.java
+++ src/Chibuzor/LargestScore.java
@@ -12,5 +12,6 @@ public class LargestScore {
             }
         }
          return largestScore;
+
     }
 }
--- tdd/Chibuzor/LargestScoreTest.java
+++ tdd/Chibuzor/LargestScoreTest.java
@@ -10,6 +10,7 @@ class LargestScoreTest {
         LargestScore largestScore = new LargestScore();
         int [] scores = {5000,10,15,20,45,66,77,80,95,120};
          assertEquals(5000,largestScore.getLargetScore(scores));
+
     }
 
 
